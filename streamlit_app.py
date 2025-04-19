3035715fc0b1f13136ca3105b47754ab34e01ba2
GeoffreyAntonio19 <geo.arifin@gmail.com>
2025-04-19 16:03:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('🎈 App Name')
+st.title('Obesity Class Prediction')
 st.header('This is a machine learning project')
 st.write('Hello world!')
