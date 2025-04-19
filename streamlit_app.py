d8ba41f0cf69e3a86276725d40404cd54fe238c1
GeoffreyAntonio19 <geo.arifin@gmail.com>
2025-04-19 15:54:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from scikit-learn import StandardScaler
 
 st.title('🎈 App Name')
-
+st.header('This is a machine learning project')
 st.write('Hello world!')
